f8933590d7140934e28fd2cd5161ceff2d4fcdac
thebestfella <thisispeter@msn.com>
2020-04-22 17:18:24 -0700

    building on local machine, use prettier

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res)=>{
-	res.send("hello world from express");
-
+app.get("/", (req, res) => {
+  res.send("hello world from express");
 });
 
-app.listen(port, ()=>console.log(`example app listening on port ${port}!`));
+app.listen(port, () => console.log(`example app listening on port ${port}!`));
