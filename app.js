b272cd71fa61c7ff61e81da40fc5e02c43fd0ff1
thebestfella <thisispeter@msn.com>
2020-04-24 15:58:01 -0700

    add /demo with customized header w/418 code

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("hello world from express");
+  console.log(`/ ${port}`);
+});
+
+app.get("/demo", (req, res) => {
+  res.set("X-full-stack", "4life"); //add header "X-full-stack", "4life" in demo under network
+  res.status(418);
+  res.send("I prefer coffee at /demo"); //screen output
+  console.log(`/demo ${port}`); //server console output
 });
 
 app.listen(port, () => console.log(`example app listening on port ${port}!`));
